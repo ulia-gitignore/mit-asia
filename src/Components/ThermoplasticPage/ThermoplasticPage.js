db6750e6a45673ca268f99393e0ebf7880f7c3c4
ulia-gitignore <ulenka_0711@mail.ru>
2025-11-24 21:55:28 +0500

    Initial commit

--- src/Components/ThermoplasticPage/ThermoplasticPage.js
+++ src/Components/ThermoplasticPage/ThermoplasticPage.js
@@ -1,5 +1,5 @@
 import React from "react";
-import { Link } from "react-router-dom";
+//import { Link } from "react-router-dom";
 import "./ThermoplasticPage.css";
 
 export default function ThermoplasticPage() {
